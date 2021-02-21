5f238af1f17dafa39a958c03f838f1b7fdbe0b64
Ali Sulieman <ahsuliem@uno.edu>
2021-02-21 12:49:12 -0600

    added error types to db layer

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 
-function DBerror(message) {
-    this.message = message;
-    this.name = 'UserException';
-}
+
+var ErrNoRecord = new Error("User does not exist")
+var ErrNoJob = new Error("User does not have a job")
+
 
 class busPatrolDB {
 
@@ -28,45 +28,64 @@ class busPatrolDB {
 
         //This should never happen but if it did, still throw error
         if (!db) {
-            errCallback(error("Database connection failed"))
+            errCallback(new Error("Database connection failed"))
         }
         //If no exception thrown then database
         //connected successfully
         this.DB = db
     }
 
+    closeConnection(errCallback) {
+
+        if (!this.DB) {
+            errCallback(new Error("There is no database connection to close"))
+        } else {
+            this.DB.close(errCallback)
+        }
+    }
+
     getUserJobByUserName(username, resultCallBack) {
 
         const err = validateString(username)
         if (err !== null) {
-            resultCallBack(null, username, err)
+            resultCallBack(null, username, newErr)
         }
 
         //Make sure there is a database connection
         //in case method used before db connect
         if (!this.DB) {
-            resultCallBack(null, username, error("There is no database connection in the class. Use one of the class connection methods"))
+            resultCallBack(null, username, new Error("There is no database connection in the class. Use one of the class connection methods"))
         }
 
-        //Prepare the statemnt to ensure now injeection occurs
-        let stmt = this.DB.prepare(`SELECT title, description
-                    FROM users LEFT JOIN jobs on users.job = jobs.id
-                    WHERE users.name = ?`, (stmt, err) => {
+        const handleStmtCallback = (stmt, err) => {
             if (err) {
                 resultCallBack(null, username, err)
             } else {
                 return stmt
             }
-        })
+        }
+
+        //Prepare the statemnt to ensure now injeection occurs
+        let stmt = this.DB.prepare(`SELECT title, description
+                    FROM users LEFT JOIN jobs on users.job = jobs.id
+                    WHERE users.name = ?`, handleStmtCallback)
 
         //query the db with the prepared statemnt
         stmt.get(username, (err, row) => {
             if (err) {
                 resultCallBack(null, username, err)
             } else {
-                resultCallBack(row, username, null)
+                //row returned but it could be empty row
+                if (!row) {
+                    resultCallBack(null, username, ErrNoRecord)
+                } else if (row.name == null && row.description == null) {
+                    resultCallBack(null, username, ErrNoJob)
+                } else {
+                    resultCallBack(row, username, null)
+                }
             }
         })
+
     }
 }
 
@@ -81,32 +100,4 @@ const validateString = (paramToValidate) => {
     }
     return null
 }
-
-const db = new busPatrolDB("./buspatrol.db")
-
-const handleConnectErrorCallback = (err) => {
-    if (err) {
-        console.log(err)
-    }
-}
-
-db.connectReadOnly(handleConnectErrorCallback)
-
-const resultCallBack = (result, username, err) => {
-    if (err) {
-        console.log("Failed to get user job by user name", err)
-    }
-    else if (result) {
-        if (result.title == null && result.description == null) {
-            console.log(username + " has no job")
-        } else {
-            console.log(result)
-        }
-    }
-    else {
-        console.log("No record exists in the database")
-    }
-}
-
-db.getUserJobByUserName("amer", resultCallBack)
 
