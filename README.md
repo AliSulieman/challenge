fe68b0add9f74240636dd57a73c33a3222d2afec
Ali Sulieman <ahsuliem@uno.edu>
2021-02-23 20:07:01 -0600

    added readme file and more checks in index file for endpoint call

---
+++ README.md
@@ -0,0 +1,13 @@
+# buspatrol-interview-challenge
+
+### INFO
+The following application is a RESTFUL GET endpoint built using Javascript, Node, and Sqlite.
+
+### Requirements to run locally
+1. Clone the repo ```git clone https://github.com/AliSulieman/challenge.git```
+2. Check if Node.Js and npm is installed. By typing node -v on terminal. 
+    If no, follow the link to link https://nodejs.org/en/ to install.
+3. Navigating to the root of the cloned repo
+3. In terminal, run ```npm install``` 
+5. To run server, run ```node index.js```
+6. Using a browser or any request editor method, call ```http://localhost:5000/users/[name]```
--- index.js
+++ index.js
@@ -16,27 +16,24 @@ const router = http.createServer(function (req, res) {
     }
 
     if (req.url === "/") {
-        handle_response(200, "Hello BusPatrol")
+        handle_response(200, { message: "Hello BusPatrol" })
         return
     }
     if (req.method != "GET") {
-        handle_response(400, "Method Not Allowed")
+        handle_response(400, { message: "Method Not Allowed" })
         return
 
     }
     if (req.url === "/users/") {
-        handle_response(400, "Must provide a name after /users/name")
+        handle_response(400, { message: "Must provide a name after /users/" })
         return
     }
 
 
     busPatrolDB.connectReadOnly(function (err) {
         if (err) {
-            res.setHeader('content-type', 'application/json');
-            res.writeHead(500)
-            res.write(JSON.stringify({
-                message: "Internal Server Error"
-            }))
+            handle_response(500, { message: "Internal Server Error" })
+            return
         }
     })
 
@@ -44,17 +41,31 @@ const router = http.createServer(function (req, res) {
     const userService = new serv.UserService(busPatrolDB)
     var baseURL = 'http://' + req.headers.host + '/';
     const requestURL = new url.URL(req.url, baseURL)
-    const urlParts = requestURL.pathname.split("/users")
+    const urlParts = requestURL.pathname.split("/")
 
-    var str = urlParts[1]
-    if (str != null) {
-        str = str.substring(1)
+    indexOfUserResource = urlParts.indexOf("users")
+    //if the users resource not requested then block it
+    if (indexOfUserResource == -1) {
+        handle_response(404, { message: "Invalid resource" })
+        return
     }
+
+    //check if anything after user is provided
+    if (indexOfUserResource + 1 >= urlParts.length) {
+        //this means only /users was provided
+        handle_response(404, { message: "Must provide a name after /users/" })
+        return
+    }
+
+
+    //Name must be after /users/ so the index of users mut be followed by the index of name
+    var str = urlParts[indexOfUserResource + 1]
     userService.handleGetUserJobByName(str, function (result) {
         handle_response(result.code, result.body)
     })
     return
 })
+console.log("Starting server at port :5000")
 router.listen(5000)
 
 
